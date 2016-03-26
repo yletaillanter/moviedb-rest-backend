9cc9a01838f15e1e53f060cdc44f9f4f454322b1
Yoann Le Taillanter <y.letaillanter@gmail.com>
2016-03-26 13:41:12 +0100

    Get a movie by id = WIP

--- app.js
+++ app.js
@@ -4,24 +4,26 @@ var app = express();
 var responseTime = require('response-time');
 var request = require('request');
 var redis = require('redis');
-var client = redis.createClient();
+var redisClient = redis.createClient();
 
-var API_KEY = "a1c65ce9d24b2d4ed117f413bb94a122"
+var API_KEY = "a1c65ce9d24b2d4ed117f413bb94a122";
+var BASE_URL = 'https://api.themoviedb.org/3/';
 
 // if an error occurs, print it to the console
-client.on('error', function (err) {
+redisClient.on('error', function (err) {
     console.log("Error " + err);
 });
 
 app.set('port', (process.env.PORT || 5000));
-
-// set up the response-time middleware
+app.listen(app.get('port'), function(){
+  console.log('Server listening on port: ', app.get('port'));
+});
 app.use(responseTime());
 
 // Search a movie by name
 app.get('/search/movie/:query', function(req, res) {
 
-    request('https://api.themoviedb.org/3/search/movie?api_key=' + API_KEY + '&query=' + req.params.query, function (error, response, body) {
+    request(BASE_URL + 'search/movie?api_key=' + API_KEY + '&query=' + req.params.query, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             res.send(body);
         } else {
@@ -31,6 +33,14 @@ app.get('/search/movie/:query', function(req, res) {
 
 });
 
-app.listen(app.get('port'), function(){
-  console.log('Server listening on port: ', app.get('port'));
+// Get a movie by id WIP §§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§
+app.get('/movie/:id', function(req, res) {
+	console.log()
+    request(BASE_URL + 'movie?api_key=' + API_KEY + '&id=' + req.params.id,function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.send(body);
+        } else {
+            res.send(error);
+        }
+    });
 });
